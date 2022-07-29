68ea8b17427d4cc3a85a242b86db0b7e93eb7a6e
fataranto <fataranto@gmail.com>
2022-07-30 00:09:49 +0200

    ok - falta bonus

---
+++ src/componentes/Testimonio.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../stylesheets/Testimonio.css';
+function Testimonio(props) {
+    return (
+    <div className='contenedor-testimonio'>
+        <img 
+        className = 'imagen-testimonio'
+        src={require(`../imagenes/testimonio-${props.img}.png`)} 
+        alt='Foto de Emma'/>
+        <div className='contenedor-texto-testimonio'>
+            <p className='nombre-testimonio'><strong>{props.name}</strong> in {props.country}</p>
+            <p className='cargo-testimonio'>{props.position} at <strong>{props.company}</strong></p>
+            <p className='texto-testimonio'>"{props.testimonial}"</p>
+        </div>
+     </div>   
+    );
+}
+
+export default Testimonio;
